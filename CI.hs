0cc592320bda2f389d6f8c1d081251e94883e398
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-07-23 16:48:23 -0400

    Work on getting CI

---
+++ CI.hs
@@ -0,0 +1,26 @@
+-- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its
+-- affiliates. All rights reserved.  SPDX-License-Identifier:
+-- (Apache-2.0 OR BSD-3-Clause)
+
+-- CI script, compatible with all of Travis, Appveyor and Azure.
+
+import Control.Monad
+import System.Directory
+import System.FilePath
+import System.IO.Extra
+import System.Info.Extra
+import System.Process.Extra
+import System.Time.Extra
+
+main :: IO ()
+main = do
+    cmd "stack --no-terminal --interleaved-output build"
+    cmd "stack exec -- hlint test"
+  where
+    cmd :: String -> IO ()
+    cmd x = do
+      putStrLn $ "\n\n# Running: " ++ x
+      hFlush stdout
+      (t, _) <- duration $ system_ x
+      putStrLn $ "# Completed in " ++ showDuration t ++ ": " ++ x ++ "\n"
+      hFlush stdout
